62fa07e7e44c2c623c6150084d054bbeeace9ab6
Sebastian Germesin <633833+neogermi@users.noreply.github.com>
2019-09-09 18:49:01 +0200

    check first if the element is available in LO (#3)
    * check if the element of unread-conversations is available
    
    * fix typo of missing innerHTML

--- webview.js
+++ webview.js
@@ -1,7 +1,12 @@
 module.exports = (Franz) => {
   function getUnreadConversations() {
     let unreadConversations = 0;
-    unreadConversations = parseInt(document.querySelector('#unread-conversations').innerHTML, 10);
+    const unreadConvElement = document.querySelector('#unread-conversations');
+    
+    // on logged-out, we do not have such an element, check first!
+    if (unreadConvElement !== null && unreadConvElement.innerHTML) {
+      unreadConversations = parseInt(unreadConvElement.innerHTML, 10);
+    }
 
     Franz.setBadge(unreadConversations);
   }
