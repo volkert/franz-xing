c947e192573d864ee42967884f05b0b23450cb21
Sebastian Germesin <633833+neogermi@users.noreply.github.com>
2019-09-09 15:37:01 +0200

    check if the element of unread-conversations is available

--- webview.js
+++ webview.js
@@ -1,7 +1,12 @@
 module.exports = (Franz) => {
   function getUnreadConversations() {
     let unreadConversations = 0;
-    unreadConversations = parseInt(document.querySelector('#unread-conversations').innerHTML, 10);
+    let unreadConvElement = document.querySelector('#unread-conversations');
+    
+    // on logged-out, we do not have such an element, check first!
+    if (unreadConvElement !== null && unreadConvElement.innerHTM) {
+      unreadConversations = parseInt(unreadConvElement.innerHTML, 10);
+    }
 
     Franz.setBadge(unreadConversations);
   }
