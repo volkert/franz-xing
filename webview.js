cf73d6b298357b7c5c0f2e3dfb4a9cb0beda8b62
Sebastian Germesin <633833+neogermi@users.noreply.github.com>
2019-09-09 15:42:00 +0200

    fix typo of missing innerHTML

--- webview.js
+++ webview.js
@@ -1,10 +1,10 @@
 module.exports = (Franz) => {
   function getUnreadConversations() {
     let unreadConversations = 0;
-    let unreadConvElement = document.querySelector('#unread-conversations');
+    const unreadConvElement = document.querySelector('#unread-conversations');
     
     // on logged-out, we do not have such an element, check first!
-    if (unreadConvElement !== null && unreadConvElement.innerHTM) {
+    if (unreadConvElement !== null && unreadConvElement.innerHTML) {
       unreadConversations = parseInt(unreadConvElement.innerHTML, 10);
     }
 
